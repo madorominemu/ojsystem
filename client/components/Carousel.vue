758e2845e84f3d9a3134433cc8dee98e1118c394
madorominemu <101652706+madorominemu@users.noreply.github.com>
2022-03-28 01:30:31 +0800

    Update Carousel.vue

--- client/components/Carousel.vue
+++ client/components/Carousel.vue
@@ -5,20 +5,13 @@
 			<div class="ml-auto flex items-center">
 				<button class="rounded text-[24px] focus:outline-none bg-fill-4 dark:bg-dark-fill-4 hover:bg-fill-3 dark:hover:bg-dark-fill-3 active:text-label-3 dark:active:text-dark-label-3 text-label-2 dark:text-dark-label-2"
 						aria-label="Prev">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em"
-						 fill="currentColor" class="">
-						<path fill-rule="evenodd"
-							  d="M14.707 16.293a1 1 0 01-1.414 1.414l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L10.414 12l4.293 4.293z"
-							  clip-rule="evenodd"></path>
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor" class="">
+						<path fill-rule="evenodd" d="M14.707 16.293a1 1 0 01-1.414 1.414l-5-5a1 1 0 010-1.414l5-5a1 1 0 111.414 1.414L10.414 12l4.293 4.293z" clip-rule="evenodd"></path>
 					</svg>
 				</button>
-				<button class="rounded text-[24px] focus:outline-none bg-fill-4 dark:bg-dark-fill-4 hover:bg-fill-3 dark:hover:bg-dark-fill-3 active:text-label-3 dark:active:text-dark-label-3 text-label-2 dark:text-dark-label-2 ml-1.5"
-						aria-label="Next">
-					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em"
-						 fill="currentColor" class="">
-						<path fill-rule="evenodd"
-							  d="M9.293 7.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414-1.414L13.586 12 9.293 7.707z"
-							  clip-rule="evenodd"></path>
+				<button class="rounded text-[24px] focus:outline-none bg-fill-4 dark:bg-dark-fill-4 hover:bg-fill-3 dark:hover:bg-dark-fill-3 active:text-label-3 dark:active:text-dark-label-3 text-label-2 dark:text-dark-label-2 ml-1.5" aria-label="Next">
+					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="1em" height="1em" fill="currentColor" class="">
+						<path fill-rule="evenodd" d="M9.293 7.707a1 1 0 011.414-1.414l5 5a1 1 0 010 1.414l-5 5a1 1 0 01-1.414-1.414L13.586 12 9.293 7.707z" clip-rule="evenodd"></path>
 					</svg>
 				</button>
 			</div>
